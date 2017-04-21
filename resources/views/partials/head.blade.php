b62b6f37e4115bc692dc851d525336b2b1e27a16
Addin Gama Bertaqwa <punyagama@gmail.com>
2017-04-21 22:43:20 +0700

    adding default template

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,33 @@
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="/plugins/images/favicon.png">
+    <title>Elite Hospital Admin Template - Hospital admin dashboard web app kit</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/plugins/bower_components/bootstrap-extension/css/bootstrap-extension.css" rel="stylesheet">
+    <!-- This is Sidebar menu CSS -->
+    <link href="/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
+    <!-- This is a Animation CSS -->
+    <link href="/css/animate.css" rel="stylesheet">
+    <!-- This is a Custom CSS -->
+    <link href="/css/style.min.css" rel="stylesheet">
+
+    <!-- color CSS you can use different color css from css/colors folder -->
+    <!-- We have chosen the skin-gray-dark (gray-dark.css) for this starter
+         page. However, you can choose any other skin from folder css / colors .
+         -->
+    <link href="/css/colors/megna.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,14 @@
+<!-- jQuery -->
+<script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="bootstrap/dist/js/tether.min.js"></script>
+<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="../plugins/bower_components/bootstrap-extension/js/bootstrap-extension.min.js"></script>
+<!-- Sidebar menu plugin JavaScript -->
+<script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+<!--Slimscroll JavaScript For custom scroll-->
+<script src="js/jquery.slimscroll.js"></script>
+<!--Wave Effects -->
+<script src="js/waves.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="js/custom.min.js"></script>
